d9768b824c32f16429dd8a250c37357a1968173b
Filip Pękala <272985@student.pwr.edu.pl>
2025-03-21 11:47:33 +0100

    Actions

---
+++ backend/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main() {
+	while(true) {
+		std::cout << "Hello, World!" << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	}
+}
