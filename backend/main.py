8f137b7747f1e2a90adb1152d8071cc39d41f71a
gizlu <git@gizlu.pl>
2025-04-22 19:58:19 +0200

    Add simple POC for survey filler

---
+++ backend/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, abort
+import json
+
+app = Flask(__name__)
+
+# Load form definitions
+# with open("forms.json") as f:
+#     FORM_DEFINITIONS = json.load(f)
+#
+forms = [
+  {
+    "name": "1.1 Przegląd statku meteorologicznego v1",
+    "activities": [
+      "Sprawdzenie stanu kadłuba i nadbudówki.",
+      "Ocena stanu instalacji elektrycznej i hydraulicznej.",
+      "Kontrola poziomu paliwa, oleju i innych płynów eksploatacyjnych.",
+      "Uruchomienie systemów pomiarowych i sprawdzenie poprawności odczytów.",
+      "Test działania systemów awaryjnych i alarmowych.",
+      "Sprawdzenie czasu reakcji systemów automatycznych.",
+      "Ocena stanu zasilania awaryjnego (baterie, agregaty).",
+      "Sprawdzenie stanu i kalibracja urządzeń meteorologicznych (np. anemometr, barometr, radar pogodowy).",
+      "Kontrola uszczelek, połączeń i pokryw zabezpieczających.",
+      "Inspekcja filtrów powietrza, wody i elementów systemu chłodzenia."
+    ]
+  }
+]
+
+
+@app.route("/form/<form_idx>")
+def form(form_idx):
+    form = forms[int(form_idx)]
+    # TODO: enable autoescape
+    return render_template("form.html.jinja", form_id=form_idx, title=form['name'], activities=form['activities'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
