65cfc7a694df417f4ed261b8679bf2ae193f639e
Filip Pekala <272985@student.pwr.edu.pl>
2025-04-23 19:45:00 +0200

    refactor(test): Change how test returns a value

--- backend/tests/surveys-to-json/test.sh
+++ backend/tests/surveys-to-json/test.sh
@@ -3,8 +3,5 @@
 json=$(./baza/surveys_to_json ./backend/tests/surveys-to-json/example.xlsx)
 json_target=$(cat ./backend/tests/surveys-to-json/example.json)
 
-if [ "$json" == "$json_target" ]; then
-	exit 0
-else
-	exit 1
-fi
+[ "$json" == "$json_target" ]
+exit $?
