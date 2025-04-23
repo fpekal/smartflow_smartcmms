cee6c150f90bc1fb47dd3373d40e7c5c443aec0b
Filip Pękala <272985@student.pwr.edu.pl>
2025-04-23 21:31:08 +0200

    cd(backend): Run backend

--- backend/main.py
+++ backend/main.py
@@ -34,4 +34,4 @@ def form(form_idx):
     return render_template("form.html.jinja", form_id=form_idx, title=form['name'], activities=form['activities'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
