e7c9cfbd0e202ecd00894eecc0703ccfd2335262
Filip Pękala <272985@student.pwr.edu.pl>
2025-05-29 08:13:00 +0200

    Add env variable for backend url

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cat <<EOF > .env
+cat <<EOF >.env
 POSTGRES_USER=test
 POSTGRES_PASSWORD=test
 POSTGRES_DB=test
 MONGO_USER=test
 MONGO_PASSWORD=test
+BACKEND_URL=http://backend:5000
 EOF
 
 docker build -t smartcmms-backend . -f backend/Dockerfile --target build
